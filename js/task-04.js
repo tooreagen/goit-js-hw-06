eb344df27daef49e52c73d0cb310ee4b6f8f064f
tooreagen <tooreagen@gmail.com>
2022-12-11 12:08:20 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -18,36 +18,3 @@ const decrement = event => {
 
 btnDecrement.addEventListener("click", decrement);
 btnIncrement.addEventListener("click", increment);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let counterValue = 0;
-
-// const btnDecrement = document.querySelector('button[data-action="decrement"]');
-// const btnIncrement = document.querySelector('button[data-action="increment"]');
-// const span = document.querySelector("#value");
-
-// const btnClick = event => {
-//     counterValue = counterValue + Number(event.currentTarget.textContent);
-//     span.textContent = counterValue;
-//     console.log("Current value = ", counterValue);
-// }
-
-// btnDecrement.addEventListener("click", btnClick);
-// btnIncrement.addEventListener("click", btnClick);
